e339593fbb1ff419559518d4daf96a80cdc01cd5
Bharath <bharathg1252001@gmail.com>
2023-01-21 09:51:16 +0530

    completed

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your code here
+let i=1
+  async function fetchByPage (){
+  const cont = document.getElementById('issue_container')
+  const pi = document.getElementById('page_indicator')
+  pi.innerText = `Page number ${i}`
+  const rawData = await fetch(`https://api.github.com/repositories/1296269/issues?page=${i}&per_page=5`)
+  const parsedData = await rawData.json()
+  let store =''
+	  // const s = new Set();
+  parsedData.forEach(issue => {
+	  // s.add(issue.title);
+	  store += `<li>${issue.title}</li>`
+  });
+
+	// s.forEach(item =>{
+	// 	store += `<li>${item}</li>`
+	// })
+	  
+  cont.innerHTML = store
+  }
+  fetchByPage(i)
+  function loadNext(){
+  i++;
+  fetchByPage()
+  }
+  function loadPrev(){
+  if(i === 1) return;
+  i--;
+  fetchByPage()
+  }
